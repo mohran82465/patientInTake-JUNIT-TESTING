da9f96c04ebd5a61a24be7cf091d679aa2b4b638
mohran <mohamed01098187312@gmail.com>
2023-09-05 12:49:39 +0300

    customizing test messages and reporting

--- src/test/java/com/mohran/DateTimeConverterShould.java
+++ src/test/java/com/mohran/DateTimeConverterShould.java
@@ -1,26 +1,57 @@
 package com.mohran;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DisplayName("DateTimeConverter should")
 class DateTimeConverterShould {
+    @Nested
+    @DisplayName("convert string with 'today' keyword correctly")
+    class todayTest{
+        @Test
+        @DisplayName(" correctly")
+        void convertCorrectPatternToDateTime(){
+            LocalDate today = LocalDate.of(2023, 9, 5);
+            LocalDateTime result = DateTimeConverter.convertStringTODateTime("today 1:00 pm",
+                    today);
+            assertEquals(result,LocalDateTime.of(2023,9,5,13,0 ),
+                    ()->"Failed to convert 'today' string to expected date time, today passed was : "+today);
+            // note that lambda expression here to speed up test execution
+
+        }
+
+        @Test
+        @DisplayName("regardless of case")
+        void convertTodayStringCorrectlyCaseInsensitive(){
+            LocalDate today = LocalDate.of(2023, 9, 5);
+            LocalDateTime result = DateTimeConverter.convertStringTODateTime("toDay 1:00 pm",
+                    today);
+            assertEquals(result,LocalDateTime.of(2023,9,5,13,0 ),
+                    ()->"Failed to convert 'today' string to expected date time, today passed was : "+today);
+            // note that lambda expression here to speed up test execution
+
+        }
+    }
+
+
     @Test
+    @DisplayName("convert expected date time pattern in string correctly ")
     void covertTodayStringCorrectly()
     {
-        LocalDateTime result = DateTimeConverter.convertStringTODateTime("today 1:00 pm", LocalDate.of(2023,9,5));
-        assertEquals(result,LocalDateTime.of(2023,9,5,13,0));
+        LocalDate today = LocalDate.of(2023, 9, 5);
+        LocalDateTime result = DateTimeConverter.convertStringTODateTime("today 1:00 pm",
+                today);
+        assertEquals(result,LocalDateTime.of(2023,9,5,13,0 ),
+                ()->"Failed to convert 'today' string to expected date time, today passed was : "+today);
+        // note that lambda expression here to speed up test execution
     }
     @Test
-    void convertCorrectPatternToDateTime(){
-        LocalDateTime result = DateTimeConverter.convertStringTODateTime("9/7/2023 1:00 pm",
-                LocalDate.of(2023,9,6));
-        assertEquals(result,LocalDateTime.of(2023,9,7,13,0));
-    }
-    @Test
+    @DisplayName("throw exception if entered pattern of st ring incorrect ")
     void throwExceptionIfIncorrectPatternProvided(){
        Throwable error =  assertThrows(RuntimeException.class,()->DateTimeConverter.convertStringTODateTime("9/7/2023 100 pm",
                 LocalDate.of(2023,9,6)));
